d8f3d97651fe09cf826dd660c747e517f51375e2
Kaai Suzuki <kaaisuzuki@Kaais-MacBook-Pro.local>
2019-01-07 23:02:56 -0800

    Understand difference between let and const

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+//let example = 'Dylan';
+const example = 'Dylan';
+
+/*
+override - overwrite example in case of using 'let'.
+not necessary to add let again like var which we used to use.
+
+but const = constant cannot be overwritten like let
+*/
+
+//example = 'Israel';
+
+console.log(example);
 
